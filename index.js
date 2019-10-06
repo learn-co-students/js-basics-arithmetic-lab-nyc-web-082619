de9db768957c7281f9867de7bd55a09555725e93
github username <github email address>
2019-10-06 15:08:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+let var1 = 2
+let var2 = 31
+
+const multiply = var1 * var2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = 18 % 7
+
+const max = Math.max(1,2,5,20)
